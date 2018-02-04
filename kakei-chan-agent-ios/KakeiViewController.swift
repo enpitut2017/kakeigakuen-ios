a65de2962c76b27623f55d3113ef5f4b3a6880ef
赤坂勝哉 <akasakatora1208@gmail.com>
2018-02-04 22:59:45 +0900

    アイコンの設定

--- kakei-chan-agent-ios/KakeiViewController.swift
+++ kakei-chan-agent-ios/KakeiViewController.swift
@@ -12,13 +12,15 @@ class KakeiViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
     }
     
 
